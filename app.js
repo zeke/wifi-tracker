a6dfb05282cb7b188fa8d0f5e8e97eba7b650bbd
Zeke Sikelianos <zeke@sikelianos.com>
2017-01-28 21:40:49 -0800

    save geodata with each network, unique by ssid

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 const wifi = require('wifi-list')
 const path = require('path')
+const geopositionToObect = require('geoposition-to-object')
 const {ipcRenderer, remote} = require('electron')
 
-function getWifiList() {
-  navigator.geolocation.getCurrentPosition((position) => {
-    wifi(function (err, list) {
-      if (err) {
-        ipcRenderer.send('error', err)
-      } else {
-        ipcRenderer.send('list', list, position)
-      }
-    })
+function check () {
+  console.log('check')
+  navigator.geolocation.getCurrentPosition(getList, positionError)
+}
+
+function positionError (err) {
+  console.error(err)
+  ipcRenderer.send('error', err)
+}
+
+function getList (position) {
+  console.log('getList', position)
+  wifi(function (err, list) {
+    if (err) {
+      console.error(err)
+      ipcRenderer.send('error', err)
+    } else {
+      ipcRenderer.send('list', list, geopositionToObect(position))
+    }
   })
 }
 
-setInterval(getWifiList, 15 * 1000)
+check()
+setInterval(check, 15 * 1000)
--- main.js
+++ main.js
@@ -1,10 +1,12 @@
+process.env.GOOGLE_API_KEY = 'AIzaSyCVQFTQJORqTZQ05lzm4eNZhdbL2rPqq2g'
+require('electron-debug')({showDevTools: true})
 const {app, BrowserWindow, ipcMain} = require('electron')
 const url = require('url')
 const path = require('path')
 const fs = require('fs')
 const dataFile = path.join(app.getPath('userData'), 'networks.json')
 const networks = fs.existsSync(dataFile) ? require(dataFile) : []
-const names = networks.map(network => network.name)
+const bssids = networks.map(network => network.bssid)
 let win = null
 
 app.once('ready', function () {
@@ -15,16 +17,17 @@ app.once('ready', function () {
   win.loadURL(`file://${path.join(__dirname, 'index.html')}`)
 })
 
-ipcMain.on('list', (event, list) => {
+ipcMain.on('list', (event, list, position) => {
   list.forEach(network => {
     if (!network || !network.name) return
-    if (names.includes(network.name)) {
+    if (bssids.includes(network.bssid)) {
       console.log(`${network.name} (already exists)`)
       return
     }
+    network.position = position
     networks.push(network)
-    names.push(network.name)
-    console.log(network.name)
+    bssids.push(network.bssid)
+    console.log(network)
     fs.writeFileSync(dataFile, JSON.stringify(networks, null, 2))
   })
 })
